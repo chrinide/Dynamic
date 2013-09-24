fdd4b6165f05f062af93fafa20671c3eab25bb1d
J. L. Holmes <jeanette.holmes@gmail.com>
2013-09-23 21:29:46 -0400

    change to a-b lists instead of full 2-d array
    to help it scale to a higher number of items and knapsack capacity,
    instead of using a full 2-d array it now uses 2 rows of the array only.
    
    For each item i, we need the array for the item i and the tem i-1 to
    figure out what the solution for each weight for item i.  Because we
    only need the array entries for i and i-1, we only use 2 rows of the
    array instead of keeping them all.  They are called solvedA and
    solvedB, and we alternate between them as we go through the list of
    items.

--- Knapsack/Knapsack.py
+++ Knapsack/Knapsack.py
@@ -1,15 +1,14 @@
 
 ## Graph operations
 items = []
-solved = {}
+solvedA = {}
+solvedB = {}
 
 
     
 def readInData (fileToRead):
-## This reads the nodes and edges from the given file and puts
-## them into the nodes dictionary and the edges list.  Initially set
-## all of the leader nodes to the negative of the node number
-## so each node is in its own cluster
+## This reads the items with  values and weights from the file and puts
+## them into the nodes dictionary and the edges list. 
 
     i = 0
     numItems = 0
@@ -41,38 +40,40 @@ def readInData (fileToRead):
     return knapsackSize
 
 def initializeSolutions (n, size):
-    for itemNumber in range (0, n):
-        solved[itemNumber] = []
     for w in range (0, size+1):
-        solved[0].append(0)
+        solvedA[w] = 0
+        solvedB[w] = 0
     return 
 
-def findSolution (i, w):
+def findSolution (i, w, usingA):
     solution = 0
-    itemValue, itemWeight = items[i]
-    iSolutions = solved[i]
-##    if w in iSolutions:
-    solution = iSolutions[w]
+
+    if usingA:
+        solution = solvedA[w]
+    else:
+        solution = solvedB[w]
+
+    
     return solution
 
-def rememberSolution (i, w, solution):
-##    if solution != 0:
-    iSolutions = solved[i]
-    iSolutions.insert(w, solution)
-##        solved[i][w] = solution
+def rememberSolution (i, w, solution, usingA):
+    if usingA:
+        solvedA[w] = solution
+    else:
+        solvedB[w] = solution
     return 
 
 
-def solveAProblem (i, w):
+def solveAProblem (i, w, usingA):
 
     itemValue, itemWeight = items[i]
-    choice1 = findSolution (i-1, w)
+    choice1 = findSolution (i-1, w, not usingA)
     choice2 = 0
-    if (w - itemWeight > 0):
-        choice2 = findSolution (i-1, w-itemWeight) + itemValue
+    if (w - itemWeight >= 0):
+        choice2 = findSolution (i-1, w-itemWeight, not usingA) + itemValue
             
     choice = max(choice1, choice2)
-    rememberSolution(i, w, choice)
+    rememberSolution(i, w, choice, usingA)
 
 
     return 
@@ -86,6 +87,7 @@ def fillKnapsack ():
     print "About to open  file "
 ##    f = open ("knapsack_big.Txt", 'r')
     f = open ("knapsack1.Txt", 'r')
+##    f = open ("knaptest1.Txt", 'r')
 
 ##  Read the data file 
     kSize = readInData (f)
@@ -95,13 +97,20 @@ def fillKnapsack ():
 
     initializeSolutions (numItems, kSize)
 
+    usingA = False
+
 
     for itemNumber in range (1, numItems):
+        usingA = not usingA
         for residualWeight in range(kSize):
-            solveAProblem (itemNumber, residualWeight)
+            solveAProblem (itemNumber, residualWeight, usingA)
+        if (itemNumber % 20 == 0):
+            print "Still working, itemNumber ", itemNumber
+        
 
-    
-    print solved[numItems-1][kSize-1]
+
+        
+    print findSolution(numItems-1,kSize-1, usingA)
 
 
 
